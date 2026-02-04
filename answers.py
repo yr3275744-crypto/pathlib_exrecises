e523ea546f42efa0e0541b40329b827b4b306a61
Ytshak_Raiyner <yr3275744@gmail.com>
2026-02-05 01:08:23 +0200

    add answer 3

--- answers.py
+++ answers.py
@@ -16,3 +16,17 @@ if file_path.exists():
 else:
     file_path.write_text("This is a new file created by pathlib.")
 print(file_path.read_text())
+
+#question 3
+def print_path(path):
+    try:
+        print(path.name)
+        print(path.stem)
+        print(path.suffix)
+        print(path.parent)
+        parents = path.parents
+        for value in parents:
+            print(value)
+    except Exception as e:
+        print(f"error! the type error:{e}")
+print_path(Path("documents/reports/annual_report.pdf"))
